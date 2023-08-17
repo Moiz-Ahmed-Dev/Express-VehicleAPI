f49c600e96bc81025ecf6041e835016ab2b9ab0c
Moiz Ahmed Khan <moizm377@gmail.com>
2023-08-17 09:22:07 -0700

    FEAT: Completed

--- API/Mailer/Router.js
+++
@@ -1,7 +0,0 @@
-const app = require('express')
-const router = app.Router()
-const { sendMail, sendFancyMail } = require('./Controller')
-
-router.post('/sendmail', sendMail)
-router.post('/sendfancymail', sendFancyMail)
-module.exports = router
--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const CategoryRouter = require('./API/Category/Router')
 const UserRouter = require('./API/User/Router')
 const ProductRouter = require('./API/Products/Router')
 const BrandRouter = require('./API/Brands/Router')
-const MailRouter = require('./API/Mailer/Router')
 const OrderRouter = require('./API/Orders/Router')
 
 const port = process.env.SERVER_PORT || 3200
@@ -15,7 +14,6 @@ app.use ('/api', CategoryRouter)
 app.use ('/api', UserRouter)
 app.use ("/api", BrandRouter)
 app.use ("/api", ProductRouter)
-app.use ("/api", MailRouter)
 app.use ("/api", OrderRouter)
 
 
